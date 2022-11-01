51ca2d1711a5da51474310c67b57fc60895c5e05
SameepAher <sameepaher@gmail.com>
2022-11-02 00:09:15 +0530

    Home Section 2 fixed

--- src/components/molecules/TransactionCard/index.js
+++ src/components/molecules/TransactionCard/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 const index = ({ picSrc, title, description }) => {
   return (
     <div className="border-2 rounded-lg px-6 py-6 hover:bg-green-300 cursor-pointer">
-      <img src={picSrc} alt="" className="mb-12 w-10 h-10" />
-      <div className="font-bold">{title}</div>
-      <div className="mt-4 text-gray-500">{description}</div>
+      <img src={picSrc} alt="" className="mb-12 w-16 h-16" />
+      <div className="font-semibold text-2xl">{title}</div>
+      <div className="mt-8 text-gray-500 text-xl lg:w-auto md:w-4/5 pb-4">{description}</div>
     </div>
   )
 }
--- src/components/organisms/HomeSection2/index.js
+++ src/components/organisms/HomeSection2/index.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import TransactionCard from 'molecules/TransactionCard'
+import card1 from '../../../images/card1.png'
+import card2 from '../../../images/card2.png'
+import card3 from '../../../images/card3.png'
+import card4 from '../../../images/card4.png'
 
 const index = () => {
   const cards = [
     {
-      picSrc: '',
+      picSrc: card1,
       title: 'Always Protected',
       description: 'Shopping with UNINE is even more secure thanks to Buyer Protection.',
     },
     {
-      picSrc: '',
-      title: 'Always Protected',
-      description: 'Shopping with UNINE is even more secure thanks to Buyer Protection.',
+      picSrc: card2,
+      title: 'Get Reward',
+      description: 'You can keep using your favorite card and keep getting rewards.',
     },
     {
-      picSrc: '',
-      title: 'Always Protected',
-      description: 'Shopping with UNINE is even more secure thanks to Buyer Protection.',
+      picSrc: card3,
+      title: 'No Hidden Fees',
+      description: 'You will only be charged when selling goods or requesting payment.',
     },
     {
-      picSrc: '',
-      title: 'Always Protected',
-      description: 'Shopping with UNINE is even more secure thanks to Buyer Protection.',
+      picSrc: card4,
+      title: 'Free shipping',
+      description: 'Changed your mind after buying? Send back the item you purchased',
     },
   ]
 
   return (
     <div className="py-2 lg:py-16">
       <div className="text-3xl font-semibold text-center mt-10">Safe & Convenient Transaction</div>
-      <div className="text-xl mx-auto md:w-full font-normal text-center mt-8 text-gray-500">
-        Want to pay anything so easy with the touch of a finger. Through <br /> UNINE, you can make
+      <div className="text-xl mx-auto md:w-2/5 w-4/5 font-normal text-center mt-8 text-gray-500">
+        Want to pay anything so easy with the touch of a finger. Through UNINE, you can make
         practically any transaction.
       </div>
-      <div className="grid lg:grid-cols-4 px-12 lg:px-44 mt-10 lg:mt-16 gap-y-4 lg:gap-x-8">
+      <div className="grid md:grid-cols-2 md:gap-x-8 lg:grid-cols-4 px-12 lg:px-44 mt-10 lg:mt-16 gap-y-10">
         {cards.map((card) => (
           <TransactionCard picSrc={card.picSrc} title={card.title} description={card.description} />
         ))}
