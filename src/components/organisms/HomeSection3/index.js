03d66137ce03f28ecdef1116a0012c2b985b1ada
SameepAher <sameepaher@gmail.com>
2022-11-02 00:40:19 +0530

    Home Section 3 & 4 fixed

--- src/components/organisms/HomeSection3/index.js
+++ src/components/organisms/HomeSection3/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import img1 from '../../../images/img1.png'
 
 const index = () => {
   return (
     <div className="mt-12 flex flex-col lg:flex-row justify-center items-center pb-12">
       <div className="w-full lg:w-2/5 p-6">
-        <div className="w-4/5 h-80 m-auto border-2 rounded-lg"></div>
+        <img src={img1} alt="" className="w-4/5 h-80 m-auto" />
       </div>
       <div className="w-4/5 mt-6 lg:mt-0 lg:w-2/5">
-        <div className="font-bold text-2xl px-6 lg:px-0">
+        <div className="font-semibold text-3xl px-6 lg:px-0 lg:w-3/5">
           Keep your cash flow clear 💸 and keep increasing 🚀
         </div>
-        <div className="text-xl text-gray-600 mt-6 px-6 lg:px-0">
+        <div className="text-2xl text-gray-600 mt-6 px-6 lg:px-0 lg:w-4/5">
           See it all at a glance when you link your cash accounts, credit cards, investments, and
           bills.
         </div>
-        <div className="mt-12 px-6 lg:px-0">
+        <div className="mt-12 px-6 text-xl lg:px-0">
           <span className="bg-green-300 py-4 px-6 rounded-lg">Try for now →</span>
         </div>
       </div>
--- src/components/organisms/HomeSection4/index.js
+++ src/components/organisms/HomeSection4/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import img2 from '../../../images/img2.png'
 
 const index = () => {
   return (
     <div className="mt-6 lg:mt-12 flex flex-col-reverse lg:flex-row justify-center items-center pb-12 lg:pb-24">
-      <div className="w-4/5 mt-6 lg:mt-0 lg:w-2/5 lg:px-24">
-        <div className="font-bold text-2xl px-6 lg:px-0">
+      <div className="w-4/5 mt-6 lg:mt-0 lg:w-2/5 lg:px-24 lg:pr-16">
+        <div className="font-semibold text-3xl px-6 lg:px-0">
           Keep your cash flow clear 💸 and keep increasing 🚀
         </div>
-        <div className="text-xl text-gray-600 mt-6 px-6 lg:px-0">
+        <div className="text-2xl text-gray-600 mt-6 px-6 lg:px-0">
           See it all at a glance when you link your cash accounts, credit cards, investments, and
           bills.
         </div>
-        <div className="mt-12 px-6 lg:px-0">
+        <div className="mt-12 px-6 text-xl lg:px-0">
           <span className="bg-green-300 py-4 px-6 rounded-lg">Try for now →</span>
         </div>
       </div>
       <div className="w-full lg:w-2/5 p-6 lg:p-0">
-        <div className="w-4/5 h-80 mx-auto lg:m-0 border-2 rounded-lg"></div>
+        <img src={img2} alt="" className="w-4/5 h-80 mx-auto lg:m-0" />
       </div>
     </div>
   )
