4b8f328784d84f16bd09c83852922bc332a9157f
SameepAher <sameepaher@gmail.com>
2022-11-02 11:51:17 +0530

    Home Section 6 fixed

--- src/components/organisms/HomeSection6/index.js
+++ src/components/organisms/HomeSection6/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import appStore from '../../../images/appStore.png'
+import playStore from '../../../images/playStore.png'
 
 const index = () => {
   return (
-    <div className="bg-green-400 lg:px-96 py-12 lg:py-16">
+    <div className="bg-green-400 lg:px-56 py-12 lg:py-16">
       <div className="">
-        <div className="mx-auto font-semibold text-3xl w-80 lg:w-96 text-center">
+        <div className="mx-auto font-semibold text-4xl px-12 lg:px-0 lg:w-3/5 text-center">
           Get Unine App on Google Play or App Store
         </div>
-        <div className="mt-6 lg:w-2/5 mx-auto text-center">
+        <div className="mt-12 lg:w-2/5 px-12 lg:px-0 mx-auto text-center">
           Build your financial literacy within a transparant community. Follow other investors,
           share insights with people.
         </div>
-        <div className="flex justify-center mt-6">
-          <div className="w-36 h-16 border-2 rounded-lg mx-4"></div>
-          <div className="w-36 h-16 border-2 rounded-lg mx-4"></div>
+        <div className="flex justify-center mt-12">
+          <img src={appStore} alt="" className="cursor-pointer mx-4" />
+          <img src={playStore} alt="" className="cursor-pointer mx-4" />
         </div>
       </div>
     </div>
