f8bd44bc8795c9b7c98a225fb9de81f3f0dad661
SameepAher <sameepaher@gmail.com>
2022-11-02 11:39:45 +0530

    Home Section 5 fixed

--- src/components/organisms/HomeSection5/index.js
+++ src/components/organisms/HomeSection5/index.js
@@ -1,15 +1,46 @@
 import React from 'react'
+import person from '../../../images/person.png'
 
 const index = () => {
   return (
-    <div className="mt-12 pb-12">
-      <div className="text-3xl py-7 text-center font-semibold">Our Customer Says</div>
-      <div className="lg:flex py-4 justify-center">
+    <div className="mt-12 pb-20">
+      <div className="text-2xl md:text-4xl py-7 text-center font-semibold">Our Customer Says</div>
+      <div className="grid lg:grid-cols-2 py-4 px-4 md:px-12 xl:px-36 gap-y-12">
         <div className="lg:px-6">
-          <div className="w-64 mx-auto lg:w-96 h-64 border-2 rounded-lg"></div>
+          <div className="mx-auto p-8 border-2 rounded-lg">
+            <div className="font-semibold text-xl lg:text-2xl">I paid off $24,000 first year</div>
+            <div className="mt-6 pb-6 border-b-2 text-lg md:text-xl">
+              “Without the system and the support, I would have been canoeing without a paddle!
+              Thanks for being my paddle, UNINE. 😉 👍 💪”
+            </div>
+            <div className="flex py-6">
+              <div className="">
+                <img src={person} alt="" className="rounded-full w-16 h-16" />
+              </div>
+              <div className="ml-4 m-auto">
+                <p>Ronald Richards</p>
+                <p>First Years of using UNINE</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="lg:px-6 mt-8 lg:mt-0">
-          <div className="w-64 mx-auto lg:w-96 h-64 border-2 rounded-lg"></div>
+        <div className="lg:px-6">
+          <div className="mx-auto p-8 border-2 rounded-lg">
+            <div className="font-semibold text-xl lg:text-2xl">My net worth is already $43k</div>
+            <div className="mt-6 pb-6 border-b-2 text-lg md:text-xl">
+              “When I got frustrated with Mint and its lack of flexibility, I found YNAB through
+              some random blog post. I never looked back. 💰”
+            </div>
+            <div className="flex py-6">
+              <div className="">
+                <img src={person} alt="" className="rounded-full w-16 h-16" />
+              </div>
+              <div className="ml-4 m-auto">
+                <p>Wade Warren</p>
+                <p>3 Years of using UNINE</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
