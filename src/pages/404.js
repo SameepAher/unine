cf3a6476f3f551603facba4d09e8fe0450022005
SameepAher <sameepaher@gmail.com>
2022-11-04 02:43:25 +0530

    404 customized

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from 'atoms/Layout'
 import Seo from 'atoms/Seo'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="text-center py-[100px] lg:py-[203.5px]">
+      <div className="font-semibold text-4xl">Page not found</div>
+      <div className="mt-12">
+        <Link to={'/'} className="rounded-xl px-4 py-4 text-gray-800 bg-green-300">
+          Go back to Home Page
+        </Link>
+      </div>
+    </div>
   </Layout>
 )
 
